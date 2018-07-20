b6fba13254aca26b1dec7623769e61b190b98214
Jim Zhan <jim.zhan@me.com>
2018-07-21 01:19:57 +0800

    feat(core): [WIP] add unified jest

---
+++ etc/jest.config.js
@@ -0,0 +1,11 @@
+console.log(process.cwd())
+
+module.export = {
+  rootDir: process.cwd(),
+  testEnvironment: 'node',
+  setupTestFrameworkScriptFile: require.resolve('./jest.setup'),
+  testMatch: [
+    '<rootDir>/__tests__/**/*.js?(x)',
+    '<rootDir>/?(*.)+(spec|test).js?(x)',
+  ]
+}
