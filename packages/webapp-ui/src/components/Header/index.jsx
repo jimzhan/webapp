062574608ea23e7b397e573d8823d9dd9f795c32
Jim Zhan <jim@EPCNSZXW0354s-Mac-mini.local>
2018-07-25 20:46:41 +0800

    fix(test): babel + ava for react

--- packages/webapp-ui/src/components/Header/index.jsx
+++ packages/webapp-ui/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Layout } from 'antd'
 
+/* eslint-disable */
 export default ({ children }) => (
   <Layout.Header>
     {children}
--- packages/webapp-ui/src/layouts/BaseLayout.jsx
+++ packages/webapp-ui/src/layouts/BaseLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Layout } from 'antd'
 import { Content, Header } from '../components'
 
+/* eslint-disable */
 export default ({ children }) => (
   <Layout>
     <Header>
